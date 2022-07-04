0039ef6a1a00ad1d131945c7715037914d873470
rendrap <rendrapradana@gmail.com>
2022-07-04 16:22:34 +0700

    add image

--- exercises/exercise-01--create-a-new-next-js-type-script-project/hello-nextjs/pages/index.tsx
+++ exercises/exercise-01--create-a-new-next-js-type-script-project/hello-nextjs/pages/index.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link'
 const Home = () => {
   return (
     <div>
+      <h2>Hello</h2>
+      <p>My name is Rendra</p>
+      <img src="/lazar.png" alt="" />
+
       <h2>My favourite music genres:</h2>
       <ul>
         <li>
