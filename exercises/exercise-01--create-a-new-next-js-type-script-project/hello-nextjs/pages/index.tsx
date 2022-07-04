73887f7ef6d1d74888dffded1e042b41add47f0d
rendrap <rendrapradana@gmail.com>
2022-07-04 20:52:53 +0700

    adding external image

--- exercises/exercise-01--create-a-new-next-js-type-script-project/hello-nextjs/pages/index.tsx
+++ exercises/exercise-01--create-a-new-next-js-type-script-project/hello-nextjs/pages/index.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+const TWITTER_IMG =
+  'https://pbs.twimg.com/profile_images/917681456884809728/4Riby-G2_400x400.jpg'
+
 const Home = () => {
   return (
     <div>
       <h2>Hello</h2>
       <p>My name is Rendra</p>
       <Image src="/lazar.png" alt="lazar" width={200} height={200} />
+      <Image
+        src={TWITTER_IMG}
+        alt="twitter img"
+        width={250}
+        height={250}
+      ></Image>
 
       <h2>My favourite music genres:</h2>
       <ul>
